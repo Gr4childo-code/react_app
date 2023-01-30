fc7efbb662c9e9ba52bc71a11e253050b3dea505
Gr4childo <samohkin.2012@gmail.com>
2023-01-30 11:13:55 +0300

    addd custom hooks

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import useAmount from '../hooks/use-amount';
+
 const Product = (props) => {
+	const { amount, decrement, increment } = useAmount(0);
+
 	return (
 		<div>
 			<p>{props.product.name}</p>
 			<p>{props.product.price} $</p>
+			<button onClick={decrement}>-</button>
+			{amount}
+			<button onClick={increment}>+</button>
 		</div>
 	);
 };
