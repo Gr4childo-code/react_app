9c44b0903ce976ae5ca60ef346465e3a035717d9
Gr4childo <samohkin.2012@gmail.com>
2023-01-30 12:44:31 +0300

    Add style product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
-import counter from '../hocs/counter';
+import Counter from '../hocs/Counter';
+
+import style from './product.module.css';
 
 const Product = (props) => {
 	const { amount, decrement, increment } = props;
 
 	return (
-		<div>
+		<div className={style.card}>
 			<p>{props.product.name}</p>
 			<p>{props.product.price} $</p>
-			<button onClick={decrement}>-</button>
-			{amount}
-			<button onClick={increment}>+</button>
+			<div className={style.counter}>
+				<button className={style.btn_counter} onClick={decrement}>
+					-
+				</button>
+				<span>{amount}</span>
+				<button className={style.btn_counter} onClick={increment}>
+					+
+				</button>
+			</div>
 		</div>
 	);
 };
 
-export default counter(Product);
+export default Counter(Product);
