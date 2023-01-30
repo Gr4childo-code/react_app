e22360134ddca15f21f1a46e63396232385c793d
Gr4childo <samohkin.2012@gmail.com>
2023-01-30 15:21:53 +0300

    h1 add Rate/Reviews and css

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import Menu from './Menu';
+import Reviews from './Reviews';
+import Rate from './Rate';
+
+import style from './restuarant.module.css';
 
 const Restaurant = (props) => {
+	let calcRating = 0;
+	if (props.activeRestaurant.reviews) {
+		const ratingSum = props.activeRestaurant.reviews.reduce(
+			(accumulator, currentValue) => accumulator + currentValue.rating,
+			0
+		);
+		calcRating = ratingSum / props.activeRestaurant.reviews.length;
+		calcRating = calcRating % 1 === 0 ? calcRating : calcRating.toFixed(1);
+	}
 	return (
 		<div>
 			<Menu menu={props.activeRestaurant.menu} />
+			<div className={style.calcRating}>
+				Средний рейтинг: <Rate rating={calcRating} />
+			</div>
+			<div>
+				<Reviews reviews={props.activeRestaurant.reviews} />
+			</div>
 		</div>
 	);
 };
