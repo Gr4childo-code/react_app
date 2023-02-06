037bb19542021f1e0c664d7d1880280b92bce479
Gr4childo <samohkin.2012@gmail.com>
2023-02-06 19:49:02 +0300

    add midleware -> logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import logger from './middleware/logger';
 import reducer from './reduser';
+
+const enchanceLogger = composeWithDevTools(logger);
 
 export const store = configureStore({
 	reducer: reducer,
